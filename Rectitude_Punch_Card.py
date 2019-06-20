49ed358fae4545e14b8a02355fef694301236686
Jiaji Gong <gongjiajiiii@gmail.com>
2019-06-20 11:18:34 +0800

    finish function

--- Rectitude_Punch_Card.py
+++ Rectitude_Punch_Card.py
@@ -1,5 +1,14 @@
+from kivy.config import Config
+Config.set('graphics', 'resizable', False)
+Config.set('graphics', 'width', '600')
+Config.set('graphics', 'height', '450')
+
 import os
 os.environ["KIVY_NO_CONSOLELOG"] = "1"
+
+from kivy.core.window import Window
+Window.clearcolor = (40/255, 44/255,52/255, 1)
+
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import Button
@@ -10,46 +19,45 @@ kivy.require('1.11.0')
 import time
 import csv
 
-
 class Rectitude_Punch_Card(App):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.icInput = TextInput(
-            font_size=50, 
-            multiline=False, 
+            foreground_color = (219/255,45/255,67/255,1),
+            background_color=( 194/255, 206/255, 215/255, 1), 
+            font_size=60, 
+            multiline=False,
             halign="center")
 
         self.label_time = Label(
             text=self.display_time(), 
-            font_size=100, 
+            color=(252/255,138/255,22/255,1),
+            font_size=50, 
             halign='auto', 
-            valign='center')
+            valign='center')                
 
     def build(self):
         box = BoxLayout(
             orientation='vertical',
-            spacing=50)
+            spacing=30)
 
         label = Label(
             text='Enter NRIC No. below:', 
-            font_size=40)
+            color=(0/255,151/255,239/255,1),
+            font_size=60)
 
         button = Button(
             text='Register Attendance', 
-            background_color=(0, 0, 1, 1), 
+            color=(30/255,228/255,147/255,1),
+            background_color=( 71/255, 69/255, 69/255, 1), 
             font_size=40, 
-            on_press=self.get_nric)
-
-        button_test = Button(
-            text='display test message', 
-            on_press=self.test)
+            on_press=self.confirm)
 
         box.add_widget(self.label_time)
         box.add_widget(label)
         box.add_widget(self.icInput)
         box.add_widget(button)
-        box.add_widget(button_test)
 
         return box
 
@@ -77,12 +85,11 @@ class Rectitude_Punch_Card(App):
     def display_time(self):
         return time.asctime(time.localtime(time.time()))
 
-    def test(self, args):
+    def confirm(self, args):
         print(self.get_time()+"       "+self.get_nric(1))
         self.submit()
 
     def get_csv(self):
-        today = self.get_day()
         path = self.get_nric(1) + ".csv"
         self.data = []
 
@@ -107,18 +114,12 @@ class Rectitude_Punch_Card(App):
             day, in, out
             20, 07:00, 19:00
             21, 06:00, 18:00
-
         2. 先读取文件, 如果没有这个文件, 创建这个空文件
-
         3. 读取文件后, 把数据复制到data里面, data是一个二维数组, 每一个元素是day数组
-
         4. day数组包含3个元素, 日子, in 和 out
-
         5. 在写入数据的时候先获取日子, 然后遍历查找这个日子, 如果有的话, 添加out
-
         6. 遍历结束, 如果没有这个日子, 说明是in. 增加一行
         '''
-
         today = self.get_day()
         path = self.get_nric(1) + ".csv"
         day = []  # day ==> ['10','08:00','18:00']
@@ -135,13 +136,11 @@ class Rectitude_Punch_Card(App):
             day.append(self.get_time())
             self.data.append(day)
 
-
         for row in self.data:
             if(len(row)>=3):
                 row[2], row[-1] = row[-1], row[2]
 
         print(self.data)
-
 
         with open(path, 'w', newline="") as f:
             writer = csv.writer(f)
@@ -155,6 +154,7 @@ class Rectitude_Punch_Card(App):
         '''
         self.get_csv()
         self.write_csv()
+        App.stop(1)
 
 
 if __name__ == '__main__':
